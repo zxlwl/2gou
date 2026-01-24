33163d692448cd510a95d73acd8de658e9aaf0c9
RT622 <127203903+RT622@users.noreply.github.com>
2026-01-25 07:07:55 +0800

    Delete src/app directory

--- src/app/(home)/config-dialog/site-settings/types.ts
+++
@@ -1,4 +0,0 @@
-export type FileItem = { type: 'file'; file: File; previewUrl: string; hash?: string } | { type: 'url'; url: string }
-export type ArtImageUploads = Record<string, FileItem>
-export type BackgroundImageUploads = Record<string, FileItem>
-export type SocialButtonImageUploads = Record<string, FileItem>
--- src/app/music/list.ts
+++
@@ -1,6 +0,0 @@
-export const list = [
-	{
-		name: '我的悲伤是水做的',
-		iframe: `<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115259171935419&bvid=BV1HrJ9zXEvF&cid=32603177515&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`
-	}
-]
