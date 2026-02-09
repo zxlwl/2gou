705d5f39d8d652ca464cceb526785c80c5ac2bdc
zx_303 <3492416851@qq.com>
2026-02-09 16:17:18 +0800

    更新页面

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -477,7 +477,7 @@ export default function BlogPage() {
 							animate={{ opacity: 1, scale: 1 }}
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
-							href='https://juejin.cn/user/2427311675422382/posts'
+							href='/'
 							target='_blank'
 							className='card text-secondary static inline-flex items-center gap-2 rounded-xl px-4 py-2 text-xs'>
 							<JuejinSVG className='h-4 w-4' />
